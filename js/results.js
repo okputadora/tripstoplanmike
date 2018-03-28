9f31cc4bd5e8d42188e9bf364df99da1b84767f0
okputadora <okputadora@gmail.com>
2018-03-28 16:26:19 -0400

    show different events for different dates

--- js/results.js
+++ js/results.js
@@ -1,5 +1,6 @@
 var displayCities = JSON.parse(localStorage.cities)
 var coordsList = []
+var eventMarkers = []
 var vacations = localStorage.vacations
 vacations = JSON.parse(vacations)
 
@@ -10,6 +11,7 @@ vacations = JSON.parse(vacations)
      getCoords(displayCities, function(){
        var map = new google.maps.Map(document.getElementById('map'), {
          zoom: 4.5,
+         mapTypeControl: false,
          center: {lat: 38.850033, lng: -97.6500523}
        });
 
@@ -37,9 +39,9 @@ vacations = JSON.parse(vacations)
            }, 200);
          })
          // map instance that is local to the init function
-         $("#map").on("click", ".datewindow", function(){
-           cityMarker.setMap(null)
+         $(document).on("click", ".datewindow", function(){
            // clear the city marker
+           cityMarker.setMap(null)
            displayEvents(map, this)
 
          })
@@ -72,69 +74,85 @@ vacations = JSON.parse(vacations)
 
    }
 
-   function displayDateWindows(marker, map){
-     var dateWindows;
-     var vacaIndex;
-     vacations.forEach(function(vacation, index){
+  function displayDateWindows(marker, map){
+
+    console.log("Displaying date window")
+    var dateWindows;
+    var vacaIndex;
+    vacations.forEach(function(vacation, index){
        if (vacation.city.cityName === marker.city){
          dateWindows = vacation.dateWindows
          vacaIndex = index
          return
        }
      })
-     var displayDateWindows = dateWindows.map(function(date){
-       var formattedStartDate = moment(date.startDate, "YYYYMMDD").format("dddd, MMMM Do, YYYY")
-       var formattedEndDate = moment(date.endDate, "YYYYMMDD").format("dddd, MMMM Do YYYY")
-       return ("<button class='btn waves-effect waves-light datewindow' id='"+vacaIndex+
-       "-"+date.startDate+"'>" + formattedStartDate + " to " + formattedEndDate + "</button></br>")
-     })
-     var buttons = displayDateWindows.join("")
-     var infoWindow = new google.maps.InfoWindow({
-       content: "<div class='center'><h3 class='cityName'>"+marker.city+"</h3>"+
-        "<p class='vacaDesc'>You have "+displayDateWindows.length+" possible vacations in "+marker.city+
-        ". Click the date window to see some events you might be interested in.</br>"+
-        buttons+"<div>"
-     })
-     infoWindow.open(map, marker);
-   }
-   function displayEvents(map, pin){
+    var displayDateWindows = dateWindows.map(function(date){
+      var formattedStartDate = moment(date.startDate, "YYYYMMDD").format("dddd, MMMM Do, YYYY")
+      var formattedEndDate = moment(date.endDate, "YYYYMMDD").format("dddd, MMMM Do YYYY")
+      return ("<button class='btn waves-effect waves-light datewindow' id='"+vacaIndex+
+      "-"+date.startDate+"'>" + formattedStartDate + " to " + formattedEndDate + "</button></br>")
+    })
+    var buttons = displayDateWindows.join("")
+
+    var dateWindowContent = "<div><h3 class='cityName'>"+marker.city+"</h3>"+buttons
+    $(".dateWindowsContainer").append(dateWindowContent)
+  }
+
+
+  function displayEvents(map, pin){
+     // clear the events
+     for (var i = 0; i < eventMarkers.length; i++) {
+       eventMarkers[i].setMap(null);
+     }
+     eventMarkers = []
       var index = pin.id.slice(0, pin.id.indexOf("-"))
       var startDate = pin.id.slice(pin.id.indexOf("-")+ 1)
       var windows = vacations[index].dateWindows
       var interests;
-      windows.forEach(function(window){
-        if (window.startDate === startDate){
-          interests = window.interests
+      windows.forEach(function(dateWindow){
+        if (dateWindow.startDate === startDate){
+          interests = dateWindow.interests
         }
       })
       var events;
       interests.forEach(function(interest){
         events = interest.events
       })
-      events.forEach(function(event){
-        var lat = parseFloat(event.lat)
-        var lng = parseFloat(event.lon)
+
+      events.forEach(function(currentEvent){
+        var lat = parseFloat(currentEvent.lat)
+        var lng = parseFloat(currentEvent.lon)
         var coords = {lat: lat, lng: lng}
         var eventMarker = new google.maps.Marker({
           position: coords,
           map: map,
-          animation: google.maps.Animation.DROP,
-          title: event.title
+          title: currentEvent.title
         })
         eventMarker.addListener("click", function(){
           // load information into info window
-          displayEventInfo(eventMarker, event)
+          displayEventInfo(eventMarker, currentEvent, map)
         })
+        eventMarkers.push(eventMarker)
       })
    }
-   function displayEventInfo(marker, event){
-     if (event.description !== null){
-       var description = event.description
+   function displayEventInfo(marker, currentEvent, map){
+     console.log("MARKER POSITION")
+     var lat = marker.getPosition().lat()
+     var lat = lat + 0.1030022
+     var lng = marker.getPosition().lng()
+     map.panTo({lat: lat, lng: lng})
+     if (currentEvent.description !== null){
+       var description = currentEvent.description
      }
      else{description = ""}
      var infoWindow = new google.maps.InfoWindow({
-       content: "<div><h4>"+event.title+"</h4><p class='truncate'>"+description+"</p></div>"
+       content: "<div><h4>"+currentEvent.title+"</h4><p class='truncate toggleText'>"+description+"</p>"+
+       "<p>"+currentEvent.startTime+"</p></div>"
      })
      infoWindow.open(map, marker)
    }
+   // toggle length of long descriptions
+   $(document).on("click", ".toggleText", function(){
+     $(this).toggleClass("truncate")
+   })
     // append each event to the map
